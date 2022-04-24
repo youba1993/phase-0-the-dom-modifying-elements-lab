c0ba826f1ab5c857a7b86ea042c8dbf7dc1f817b
youbaidjis <youba.etud@gmail.com>
2022-04-24 04:10:27 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+document.getElementById("main").remove();
+const newHeader = document.createElement("h1")
+newHeader.setAttribute("id", "victory")
+//document.body.append(newHeader) //when i use this method it's not passinf the test but in the navigator working well
+newHeader.innerHTML = "<h1 id=\"victory\">Youba is the champion</h1>";
+
